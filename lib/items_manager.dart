aa0be9632739b8ed347b65c93e40aa23c1cf083d
Arthur Chéramy <arthur.cheramy@gmail.com>
2019-03-23 16:15:41 -0400

    StartingItem can now be specified

--- lib/items_manager.dart
+++ lib/items_manager.dart
@@ -4,6 +4,9 @@ import './item_control.dart';
 import './items.dart';
 
 class ItemsManager extends StatefulWidget {
+  final String startingItem;
+  ItemsManager({this.startingItem});
+
   @override
   State<StatefulWidget> createState() {
     return _ItemsManagerState();
@@ -13,7 +16,15 @@ class ItemsManager extends StatefulWidget {
 class _ItemsManagerState extends State<ItemsManager> {
   // _items could be final because we never assign a new List to _items, we only add items to the existing List.
   // Const (before the []) on the other hand would be problematic
-  List<String> _items = ['Item 1'];
+  List<String> _items = [];
+
+  @override
+  void initState() {
+    if (widget.startingItem != null) {
+      _items.add(widget.startingItem);
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
