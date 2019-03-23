c8ade355c26ec9e70c263ea70ef811c83011304f
Arthur Chéramy <arthur.cheramy@gmail.com>
2019-03-23 18:27:53 -0400

    Add hamburger menu

--- lib/pages/items.dart
+++ lib/pages/items.dart
@@ -6,6 +6,19 @@ class ItemsPages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage item'),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text("Flutter experiment"),
       ),
