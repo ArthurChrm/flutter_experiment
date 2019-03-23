b7f117f003cea686d0243fbfc042b837601d1032
Arthur Chéramy <arthur.cheramy@gmail.com>
2019-03-23 17:04:22 -0400

    A button allow the user to go back

--- lib/pages/item.dart
+++ lib/pages/item.dart
@@ -4,12 +4,21 @@ class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Item detail"),
-      ),
-      body: Center(
-        child: Text('On the item page'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Item detail"),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text('On the item page'),
+              RaisedButton(
+                child: Text("Back"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
