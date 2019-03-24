acb9c3f060926f6ddd8e703d8d4f6ee79edddb1d
Arthur Chéramy <arthur.cheramy@gmail.com>
2019-03-24 16:00:25 -0400

    Add TextInputs

--- lib/pages/item_create.dart
+++ lib/pages/item_create.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ItemCreatePage extends StatelessWidget {
+class ItemCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ItemCreatePageState();
+  }
+}
+
+class _ItemCreatePageState extends State<ItemCreatePage> {
+  String contentText = '';
+  String description = '';
+  double price = 0.0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Save'),
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context) {
-              return Center(
-                child: Text('This is a modal'),
-              );
-            },
-          );
-        },
-      ),
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              contentText = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.multiline,
+          onChanged: (String value) {
+            setState(() {
+              description = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              price = double.parse(value);
+            });
+          },
+        ),
+      ],
     );
   }
 }
