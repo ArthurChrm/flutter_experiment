bd8531aeabd0ae02da12464bca9e2e7cb9ef977e
Arthur Chéramy <arthur.cheramy@gmail.com>
2019-03-24 12:16:28 -0400

    Switching tabs now working

--- lib/pages/item_admin.dart
+++ lib/pages/item_admin.dart
@@ -1,52 +1,58 @@
 import 'package:flutter/material.dart';
 
 import './items.dart';
+import './item_create.dart';
+import './item_list.dart';
 
 class ItemAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Manage items'),
-            bottom: TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.create),
-                  text: 'Create item',
-                ),
-                Tab(
-                  icon: Icon(Icons.list),
-                  text: 'My items',
-                )
-              ],
-            ),
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Manage items'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: 'Create item',
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+                text: 'My items',
+              )
+            ],
           ),
-          drawer: Drawer(
-            child: Column(
-              children: <Widget>[
-                AppBar(
-                  automaticallyImplyLeading: false,
-                  title: Text('Choose'),
-                ),
-                ListTile(
-                  title: Text('All items'),
-                  onTap: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (BuildContext context) {
-                        return ItemsPages();
-                      }),
-                    );
-                  },
-                ),
-              ],
-            ),
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('All items'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (BuildContext context) {
+                      return ItemsPages();
+                    }),
+                  );
+                },
+              ),
+            ],
           ),
-          body: Center(
-            child: Text('You are on the item admin page'),
-          ),
-        ));
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ItemCreatePage(),
+            ItemListPage(),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/item_create.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItemCreatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Create an item'),
+    );
+  }
+}
