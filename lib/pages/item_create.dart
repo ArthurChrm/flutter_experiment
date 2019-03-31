35f6e77fa3d89f6f9f41f71ea9cf3f2176125389
Arthur Chéramy <arthur.cheramy@gmail.com>
2019-03-31 00:42:16 -0400

    Creation of different methods for item_create

--- lib/pages/item_create.dart
+++ lib/pages/item_create.dart
@@ -16,54 +16,68 @@ class _ItemCreatePageState extends State<ItemCreatePage> {
   String _description = '';
   double _price = 0.0;
 
+  Widget _buildTitleTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Item title'),
+      onChanged: (String value) {
+        setState(() {
+          _title = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildDescriptionTextField() {
+    return TextField(
+      maxLines: 4,
+      decoration: InputDecoration(labelText: 'Description'),
+      keyboardType: TextInputType.multiline,
+      onChanged: (String value) {
+        setState(() {
+          _description = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildPriceTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Price'),
+      keyboardType: TextInputType.number,
+      onChanged: (String value) {
+        setState(() {
+          _price = double.parse(value);
+        });
+      },
+    );
+  }
+
+  void _submitForm() {
+    final Map<String, dynamic> item = {
+      'title': _title,
+      'description': _description,
+      'price': _price,
+      'image': 'assets/baguette.jpg'
+    };
+    widget.addItem(item);
+    Navigator.pushReplacementNamed(context, '/items');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(10.0),
       child: ListView(
         children: <Widget>[
-          TextField(
-            decoration: InputDecoration(labelText: 'Item title'),
-            onChanged: (String value) {
-              setState(() {
-                _title = value;
-              });
-            },
-          ),
-          TextField(
-            maxLines: 4,
-            decoration: InputDecoration(labelText: 'Description'),
-            keyboardType: TextInputType.multiline,
-            onChanged: (String value) {
-              setState(() {
-                _description = value;
-              });
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(labelText: 'Price'),
-            keyboardType: TextInputType.number,
-            onChanged: (String value) {
-              setState(() {
-                _price = double.parse(value);
-              });
-            },
-          ),
+          _buildTitleTextField(),
+          _buildDescriptionTextField(),
+          _buildPriceTextField(),
           SizedBox(
             height: 10.0,
           ),
           RaisedButton(
             child: Text('Save'),
-            onPressed: () {
-              final Map<String, dynamic> item = {
-                'title': _title,
-                'description': _description,
-                'price': _price,
-                'image': 'assets/baguette.jpg'
-              };
-              widget.addItem(item);
-              Navigator.pushReplacementNamed(context, '/items');
-            },
+            onPressed: _submitForm,
           ),
         ],
       ),
